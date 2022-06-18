867c022eb2db06414c5c59d8a5b59883a29bdfd3
Seungbae.yu <72970043+dbadoy@users.noreply.github.com>
2022-06-19 00:53:07 +0900

    basic: add case. check odd number and abs op.

--- basic.go
+++ basic.go
@@ -17,4 +17,19 @@ func main() {
 	// check 2^k
 	// if zero, it's true
 	fmt.Println((x << 5) & ((x << 5) - 1))
+	
+	// check odd number
+	fmt.Println((79 & 1) == 1)
+
+	// check sign
+	y := -91
+	z := 30
+	// false
+	fmt.Println((y ^ z) >= 0)
+
+	// abs
+	// -71
+	fmt.Println(71 ^ -1 + 1)
+	// 101
+	fmt.Println(-101 ^ -1 + 1)
 }
