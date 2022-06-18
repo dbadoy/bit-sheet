e8afa97e24280d3a4a03a3faa39ed7b39fbe4b18
ray.yu <ray.yu@themedium.io>
2022-06-18 23:30:01 +0900

    add flag

---
+++ flag/flag.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const (
+	A_FLAG = 0x01
+	B_FLAG = 0x02
+	C_FLAG = 0x04
+	D_FLAG = 0x08
+	E_FLAG = 0x010
+)
+
+// present by bit shift
+const (
+	A_FLAG_SHIFT = (1 << 0)
+	B_FLAG_SHIFT = (1 << 1)
+	C_FLAG_SHIFT = (1 << 2)
+	D_FLAG_SHIFT = (1 << 3)
+	E_FLAG_SHIFT = (1 << 4)
+)
+
+func main() {
+	fmt.Println(A_FLAG)
+	fmt.Println(B_FLAG)
+	fmt.Println(C_FLAG)
+	fmt.Println(D_FLAG)
+	fmt.Println(E_FLAG)
+
+	fmt.Println(A_FLAG_SHIFT)
+	fmt.Println(B_FLAG_SHIFT)
+	fmt.Println(C_FLAG_SHIFT)
+	fmt.Println(D_FLAG_SHIFT)
+	fmt.Println(E_FLAG_SHIFT)
+}
