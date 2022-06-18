3115da8d96b150698903c5d5b8364cdc4c4ae331
Seungbae.yu <72970043+dbadoy@users.noreply.github.com>
2022-06-18 23:59:52 +0900

    add file basic.go.

---
+++ basic.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+  // a << n (a * 2^n)
+  // a >> n (a / 2^n)
+  
+  x := int32(1)
+
+	// 000000000100000
+	fmt.Printf("%b\n", (x << 5))
+
+	// 000000000011111
+	fmt.Printf("%b\n", ((x << 5) - 1))
+
+	// check 2^k
+	// if zero, it's true
+	fmt.Println((x << 5) & ((x << 5) - 1))
+  
+}
