6e55d44293c6a3eab1b1e9a3278fd06e785d120d
Seungbae.yu <72970043+dbadoy@users.noreply.github.com>
2022-06-19 00:40:13 +0900

    basic: typo

--- basic.go
+++ basic.go
@@ -1,19 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-  // a << n (a * 2^n)
-  // a >> n (a / 2^n)
-  
-  x := int32(1)
+	// a << n (a * 2^n)
+	// a >> n (a / 2^n)
 
-  // 000000000100000
-  fmt.Printf("%b\n", (x << 5))
+	x := int32(1)
 
-  // 000000000011111
-  fmt.Printf("%b\n", ((x << 5) - 1))
+	// 000000000100000
+	fmt.Printf("%b\n", (x << 5))
 
-  // check 2^k
-  // if zero, it's true
-  fmt.Println((x << 5) & ((x << 5) - 1))
-  
+	// 000000000011111
+	fmt.Printf("%b\n", ((x << 5) - 1))
+
+	// check 2^k
+	// if zero, it's true
+	fmt.Println((x << 5) & ((x << 5) - 1))
 }
