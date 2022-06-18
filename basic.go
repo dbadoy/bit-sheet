8ff26f14fb1409164e346b0b67c888bd02f73d8d
Seungbae.yu <72970043+dbadoy@users.noreply.github.com>
2022-06-19 00:00:47 +0900

    remove whitespace.

--- basic.go
+++ basic.go
@@ -6,14 +6,14 @@ func main() {
   
   x := int32(1)
 
-	// 000000000100000
-	fmt.Printf("%b\n", (x << 5))
+  // 000000000100000
+  fmt.Printf("%b\n", (x << 5))
 
-	// 000000000011111
-	fmt.Printf("%b\n", ((x << 5) - 1))
+  // 000000000011111
+  fmt.Printf("%b\n", ((x << 5) - 1))
 
-	// check 2^k
-	// if zero, it's true
-	fmt.Println((x << 5) & ((x << 5) - 1))
+  // check 2^k
+  // if zero, it's true
+  fmt.Println((x << 5) & ((x << 5) - 1))
   
 }
