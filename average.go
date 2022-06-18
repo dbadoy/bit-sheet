e2fb69a9d6d21248742b5883431abd0a690f7cf8
Seungbae.yu <72970043+dbadoy@users.noreply.github.com>
2022-06-19 00:40:36 +0900

    average: typo

--- average.go
+++ average.go
@@ -1,21 +1,23 @@
 package main
 
+import "fmt"
+
 func Average() {
-  x := int32(8669)
-  y := int32(32318)
-  
-  // (x + y) / 2
-  // 20493.5
-  
-  // 20493
-  // TODO: add comment
-  fmt.Println((x + y) >> 1)
-  
-  // floor
-  // 20493
-  fmt.Println((x & y) + ((x ^ y) >> 1))
-  
-  // ceiling
-  // 20494
-  fmt.Println((x | y) - ((x ^ y) >> 1))
+	x := int32(8669)
+	y := int32(32318)
+
+	// (x + y) / 2
+	// 20493.5
+
+	// 20493
+	// TODO: add comment
+	fmt.Println((x + y) >> 1)
+
+	// floor
+	// 20493
+	fmt.Println((x & y) + ((x ^ y) >> 1))
+
+	// ceiling
+	// 20494
+	fmt.Println((x | y) - ((x ^ y) >> 1))
 }
